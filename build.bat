0aa37e205a279d507f8ef4a1d4e23e74bac1d3e8
Brian Petkovsek <16124109+BrianPetkovsek@users.noreply.github.com>
2021-06-19 08:13:13 -0400

    Update build.bat

--- build.bat
+++ build.bat
@@ -1,3 +1,3 @@
 del /s /q "./dist"
 copy "./cookies.json" "./dist/cookies.json"
-nexe -n main.js -o dist/main --resources "node_modules"
+nexe -n main.js -o dist/main --resources "node_modules" --resource "1x1.png"
