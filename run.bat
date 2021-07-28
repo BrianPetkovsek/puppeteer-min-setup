0cd64283aa44fe9de33522f7411e8ca0bdada80c
BrianPetkovsek <16124109+BrianPetkovsek@users.noreply.github.com>
2021-07-27 22:27:09 -0400

    Update run.bat

--- run.bat
+++ run.bat
@@ -1,2 +1,2 @@
 @echo off
-for /f "tokens=1 delims={" %%a in ('TYPE arguments.txt') do (cmd /c node main.js %%a %*)
+for /f "tokens=1 delims={" %%a in ('TYPE arguments.txt') do (cmd /c node . %%a %*)
